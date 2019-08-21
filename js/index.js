d68e3b23b40bc29b6662a515d8618b44af6e7fb2
Alissa <alissaawoo@gmail.com>
2019-08-20 18:29:55 -0700

    changed format to use grid

---
+++ js/index.js
@@ -0,0 +1,37 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+$(document).ready(function() {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+});
+
+$("button").click(function() {
+    $('html,body').animate({
+            scrollTop: $("#page2").offset().top},
+        'slow');
+});
+
+
+
+
+
+
